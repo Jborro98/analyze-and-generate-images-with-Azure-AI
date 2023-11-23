0967a6e6660c31187950fca0fddd50d02716967e
Jonathan Borroel <jonathanb098@gmail.com>
2023-11-23 14:41:11 -0800

    Add title, textbox and 2 buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [imageRequest, setImageRequest] = useState('');
+
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handleImageRequestChange = (event) => {
+    setImageRequest(event.target.value);
+  };
+
+  const handleImageAnalysis = () => {
+    // Code to trigger image analysis
+    console.log('Image analysis triggered');
+  };
+
+  const handleImageGeneration = () => {
+    // Code to trigger image generation
+    console.log('Image generation triggered');
+  };
+
+  return (
+    <div>
+      <h1>Image Analysis and Generation</h1>
+      <input type="text" value={imageUrl} onChange={handleImageUrlChange} placeholder="Enter image URL" />
+      <input type="text" value={imageRequest} onChange={handleImageRequestChange} placeholder="Enter image request" />
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
