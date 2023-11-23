afcb66e9485ad887d70217ecf83930f1c8bb99ef
Jonathan Borroel <jonathanb098@gmail.com>
2023-11-23 14:57:56 -0800

    Add again the same but different

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [imageUrl, setImageUrl] = useState('');
-  const [imageRequest, setImageRequest] = useState('');
-
-  const handleImageUrlChange = (event) => {
-    setImageUrl(event.target.value);
-  };
-
-  const handleImageRequestChange = (event) => {
-    setImageRequest(event.target.value);
-  };
-
-  const handleImageAnalysis = () => {
-    // Code to trigger image analysis
-    console.log('Image analysis triggered');
-  };
-
-  const handleImageGeneration = () => {
-    // Code to trigger image generation
-    console.log('Image generation triggered');
-  };
-
   return (
     <div>
-      <h1>Image Analysis and Generation</h1>
-      <input type="text" value={imageUrl} onChange={handleImageUrlChange} placeholder="Enter image URL" />
-      <input type="text" value={imageRequest} onChange={handleImageRequestChange} placeholder="Enter image request" />
-      <button onClick={handleImageAnalysis}>Analyze Image</button>
-      <button onClick={handleImageGeneration}>Generate Image</button>
+      <h1>Azure Vision IA</h1>
+      <input type="text" placeholder="Enter your name" />
+      <button>Analyze</button>
+      <button>Generate</button>
     </div>
   );
 }
